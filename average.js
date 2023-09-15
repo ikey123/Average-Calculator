c6aefe1893a9009a11616e93ca3c299526f97c06
ikey123 <144579334+ikey123@users.noreply.github.com>
2023-09-15 20:30:34 +0800

    Update average.js

--- average.js
+++ average.js
@@ -1,24 +1,29 @@
 var nums = document.getElementsByClassName("num");
 var numbers = Array.prototype.slice.call(nums);
 var average = document.getElementById("average");
-var inputs = document.getElementsByClassName("inputs");
+
 $("div").on("click", ".remove", function(event){
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
+        // 更新 numbers 数组
+        nums = document.getElementsByClassName("num");
         numbers = Array.prototype.slice.call(nums);
     });
     event.stopPropagation();
 });
+
 $(".add").on("click", function(event){
   $(".inputsContainer").append("<div class='inputs'><input type='number' class='num'><button class='remove btn btn-danger'>Remove</button></div>");
+  // 更新 numbers 数组
+  nums = document.getElementsByClassName("num");
   numbers = Array.prototype.slice.call(nums);
 });
+
 function calculate() {
   var total = 0;
-  var v = 0;
-  for(var i = 0; i < inputs.length; i++) {
+  for(var i = 0; i < numbers.length; i++) {
     total += Number(numbers[i].value);
   }
-  var avg = total/inputs.length;
-  average.textContent = "Average number is: " + avg;
-};
+  var avg = total / numbers.length;
+  average.textContent = "Average number is: " + avg.toFixed(2); // 保留两位小数
+}
